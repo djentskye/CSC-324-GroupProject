be56ba97a44abcbfc9ea6a542c94f5f9cb0ab366
zhefty <zahefty@gmail.com>
2022-11-04 15:20:12 -0500

    Images as backgrounds for the buttons

--- ui.R
+++ ui.R
@@ -6,16 +6,16 @@ ui <- fluidPage(
     type = "hidden",
     tabPanel("page_I", 
              "Intro Page",
-             actionButton("page_I_E1", "Exhibit 1", icon = icon("table")),
-             actionButton( "page_I_E2", "Exhibit 2")
+             actionButton("page_I_E1", "Exhibit 1", style = "width: 200px; height: 50px; background: url('Color-blue.jfif'); background-size: cover; background-position: center;"),
+             actionButton( "page_I_E2", "Exhibit 2", style = "width: 200px; height: 50px; background: url('Color-blue.jfif'); background-size: cover; background-position: center;")
     ),
     tabPanel("page_E1", 
              "Exhibit 1",
-             actionButton("page_E1_I", "back")
+             actionButton("page_E1_I", "back", style = "width: 200px; height: 50px; background: url('Color-blue.jfif'); background-size: cover; background-position: center;")
     ),
     tabPanel("page_E2", 
              "Exhibit 2",
-             actionButton("page_E2_I", "back")
+             actionButton("page_E2_I", "back", style = "width: 200px; height: 50px; background: url('Color-blue.jfif'); background-size: cover; background-position: center;")
     )
   )
 )
