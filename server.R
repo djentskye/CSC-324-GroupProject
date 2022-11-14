a73af788d066eda4562c770fbb59438f8de3f354
zhefty <zahefty@gmail.com>
2022-11-13 23:17:57 -0600

    Functionality for most of the wireframe

--- server.R
+++ server.R
@@ -9,15 +9,55 @@
 
 library(shiny)
 
-# Define server logic required to draw a histogram
+# Define server logic
 server <- function(input, output, session) {
   switch_page <- function(i) {
     updateTabsetPanel(inputId = "wizard", selected = paste0("page_", i))
   }
   
-  observeEvent(input$page_I_E1, switch_page("E1"))
-  observeEvent(input$page_I_E2, switch_page("E2"))
-  observeEvent(input$page_E1_I, switch_page("I"))
-  observeEvent(input$page_E2_I, switch_page("I"))
+  observeEvent(input$page_A_M, switch_page("M"))
+  
+  # Door stuff
+  # Map page
+  observeEvent(input$d_page_M_E1, switch_page("E1"))
+  observeEvent(input$d_page_M_E2, switch_page("E2"))
+  observeEvent(input$d_page_M_E3, switch_page("E3"))
+  # E1
+  observeEvent(input$d_page_E1_E2, switch_page("E2"))
+  observeEvent(input$d_page_E1_E3, switch_page("E3"))
+  # E2
+  observeEvent(input$d_page_E2_E1, switch_page("E1"))
+  observeEvent(input$d_page_E2_E3, switch_page("E3"))
+  # E3
+  observeEvent(input$d_page_E3_E1, switch_page("E1"))
+  observeEvent(input$d_page_E3_E2, switch_page("E2"))
+  
+  
+  # Navigation button stuff
+  # About
+  observeEvent(input$n_page_A_E1, switch_page("E1"))
+  observeEvent(input$n_page_A_E2, switch_page("E2"))
+  observeEvent(input$n_page_A_E3, switch_page("E3"))
+  observeEvent(input$n_page_A_M, switch_page("M"))
+  # Map
+  observeEvent(input$n_page_M_A, switch_page("A"))
+  observeEvent(input$n_page_M_E1, switch_page("E1"))
+  observeEvent(input$n_page_M_E2, switch_page("E2"))
+  observeEvent(input$n_page_M_E3, switch_page("E3"))
+  # E1
+  observeEvent(input$n_page_E1_A, switch_page("A"))
+  observeEvent(input$n_page_E1_E2, switch_page("E2"))
+  observeEvent(input$n_page_E1_E3, switch_page("E3"))
+  observeEvent(input$n_page_E1_M, switch_page("M"))
+  # E2
+  observeEvent(input$n_page_E2_A, switch_page("A"))
+  observeEvent(input$n_page_E2_E1, switch_page("E1"))
+  observeEvent(input$n_page_E2_E3, switch_page("E3"))
+  observeEvent(input$n_page_E2_M, switch_page("M"))
+  # E3
+  observeEvent(input$n_page_E3_A, switch_page("A"))
+  observeEvent(input$n_page_E3_E1, switch_page("E1"))
+  observeEvent(input$n_page_E3_E2, switch_page("E2"))
+  observeEvent(input$n_page_E3_M, switch_page("M"))
 }
 
